99164fc0f40f789d42124dd444ef451dc8a4f5df
pliniogim <pliniogimenez@gmail.com>
2024-12-19 18:05:21 -0300

    third

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,19 @@
 import 'package:prompter_plg/src/terminal.dart';
+import 'package:prompter_plg/src/option.dart';
 
 void main() {
   var trm = Terminal();
-  trm.printPrompt('Test');
+
+  var options = [
+    new Option('I want red', '#00f'),
+    new Option('I want blue', '#00f'),
+  ];
+
+
+  trm.clearScreen();
+  trm.printPrompt('Options test');
+  trm.printOptions(options);
+  String answer = trm.collectInput();
+  trm.printPrompt(answer);
+  // trm.clearScreen();
 }
--- lib/src/terminal.dart
+++ lib/src/terminal.dart
@@ -1,7 +1,40 @@
 import 'dart:io';
+import 'option.dart';
 
 class Terminal {
-  void printPrompt(String prompt){
+  void printPrompt(String prompt) {
     stdout.writeln(prompt);
+  }
+
+  void printOptions(List<Option> options) {
+    
+    // for (var i = 0; i < options.length; i++) {
+    //   stdout.writeln('[$i] - ${options[i].label}');
+    // }
+    
+    options.asMap().forEach((index, option){
+      stdout.writeln('[$index] - ${option.label}');
+    });
+
+    stdout.writeln('Enter a choice: ');
+    stdout.write('>');
+  }
+
+  String collectInput() {
+    String? answer =
+        stdin.readLineSync(encoding: SystemEncoding(), retainNewlines: false);
+    if (answer!.isNotEmpty) {
+      return answer;
+    } else {
+      return "Nothing typed";
+    }
+  }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+      stdout.write('\x1B[2J\x1B[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3J\x1B[H');
+    }
   }
 }
