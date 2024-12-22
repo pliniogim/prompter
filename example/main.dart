c8c0967fb61812db4b6954e528ec2b4aa436b32d
pliniogim <pliniogimenez@gmail.com>
2024-12-22 18:36:30 -0300

    fifth

--- example/main.dart
+++ example/main.dart
@@ -1,15 +1,21 @@
-import 'package:prompter_plg/src/prompter.dart';
-import 'package:prompter_plg/src/option.dart';
+import "package:prompter_plg/prompter_plg.dart";
 
 void main() {
+  // options type - Class Option (assertion - value)
   var options = [
     new Option('I want red', '#f00'),
     new Option('I want blue', '#00f'),
   ];
 
+  // prompter class
   final prompter = new Prompter();
-
-  final usersPick = prompter.ask("What color do you like?", options);
-
-  print(usersPick);
+  
+  // multiple choice example
+  String colorCode = prompter.askMultiple("What color do you like?", options);
+  
+  // binary choice example
+  bool answer = prompter.askBinary('Do you like DART? ');
+  
+  print(colorCode);
+  print(answer);
 }
--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,16 +4,29 @@ import 'option.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  ask(String prompt, List<Option> options) {
-    _terminal.printPrompt(prompt);
-    _terminal.printOptions(options);
 
-    final input = _terminal.collectInput();
+
+  bool askBinary(String prompt){
+
+    String input = _ask('$prompt (Y/n)', []);
+    return input.contains('y');
+
+  }
+
+  askMultiple(String prompt, List<Option> options) {
+    String input = _ask(prompt, options);
     try {
       return options[int.parse(input)].value;
     } catch (err) {
       _terminal.clearScreen();
-      return ask(prompt, options);
+      return askMultiple(prompt, options);
     }
+  }
+
+  String _ask(String prompt, List<Option> options){
+    _terminal.clearScreen();
+    _terminal.printPrompt(prompt);
+    _terminal.printOptions(options);
+    return _terminal.collectInput();
   }
 }
